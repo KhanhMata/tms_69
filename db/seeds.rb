9b9270883e479fef65d4074823aad5ed15635c27
Khanh Mata <radnvk@gmail.com>
2016-10-25 14:16:45 +0700

    Fix paginate, refactors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+30.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  chatwork = "Khanh#{n+1}"
+  password = "password"
+  answer_type = 1
+  content = "Question no #{n+1}"
+  user_id = n+1
+  level_id = 2
+  content2 = "Answer no #{n+1}"
+  content3 = "Answer 2nd #{n+1}"
+  content4 = "Answer 3rd #{n+1}"
+  content5 = "Answer 4th #{n+1}"
+  is_correct = 1
+  is_correct2 = 0
+  question_id = n+13
+  Question.create!(
+                          answer_type: answer_type,
+                          content: content,
+                          level_id: level_id)
+  Answer.create!(content: content2,
+                        is_correct: is_correct,
+                        question_id: question_id)
+  Answer.create!(content: content3,
+                        is_correct: is_correct2,
+                        question_id: question_id)
+  Answer.create!(content: content4,
+                        is_correct: is_correct2,
+                        question_id: question_id)
+  Answer.create!(content: content5,
+                        is_correct: is_correct2,
+                        question_id: question_id)
+end
